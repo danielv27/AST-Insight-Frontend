d4f6b013c2311323885da947c482ba6df11f59a7
Daniel Verner <danielv2000@gmail.com>
2024-07-15 16:20:27 +0200

    Added error handling and suggestions styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
+  <h1>AST Insight</h1>
+  <div class="content-wrapper">
     <div class="left-pane">
       <CodeEditor v-model="program"/>
       <button @click="getSuggestions">Analyze</button>
     </div>
-  <div>
-    
+    <div class="right-pane">
+      <div v-if="loading">Loading...</div>
+      <div v-else-if="error">
+        <div>Something went wrong...</div> 
+        <div>Stack trace:</div> 
+        <CodeEditor :height="250" disabled v-model="error"/>
+      </div>
+      
+      <div class="suggestion-wrapper" v-else-if="suggestions">
+        <div v-for="suggestion of suggestions">
+          <CodeEditor :height="150" disabled v-model="suggestion.code"/>
+          <div>{{ suggestion.description }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,34 +27,67 @@
 import { ref } from 'vue';
 import CodeEditor from './components/CodeEditor.vue'
 
+interface Suggestion {
+  code: string;
+  description: string;
+}
 
 const program = ref(
 `int main()
 {
   int buf[32];
   int i = 45;
-
   buf[i] = 9;
 }`
 );
 
+const loading = ref(false);
+const error = ref('');
+const suggestions = ref<Suggestion[]>([]);
 
-async function getSuggestions() {
-  fetch('http://127.0.0.1:5000/analyze', {
+
+function getSuggestions() {
+  loading.value = true;
+  const url = 'http://127.0.0.1:5000/analyze';
+  fetch(url, {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({ code: program.value })
-  })
+  }).then(async (response) => {
+    const json = await response.json();
+    if(response.ok){
+      suggestions.value = json;
+      error.value = '';
+    } else {
+      error.value = json.error;
+    }
+    loading.value = false;
+  });
 }
 
 </script>
 
 <style scoped>
 
+.content-wrapper {
+  display: flex;
+  gap: 12px;
+}
+
 .left-pane {
-  width: 50%;
+  width: 40%;
+}
+
+.right-pane {
+  max-width: 40%;
+}
+
+.suggestion-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 }
 
 </style>
